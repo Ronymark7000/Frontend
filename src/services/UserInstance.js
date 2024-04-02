77ea1a008d0491216ea94d9ab7450bacfb4b7d11
Ronymark7000 <shkyaroheet7000@gmail.com>
2024-04-02 21:02:40 +0545

    Code Transfered From Mac to Asus. Succesfully Reinitialized code to preswnt working stage.

--- src/services/UserInstance.js
+++ src/services/UserInstance.js
@@ -1,4 +1,5 @@
 import axiosInstance from "../../axiosInstance";
+import { emitInfoToast } from "../site/components/Toast/EmitToast";
 
 export const getUsers = async () => {
     return axiosInstance.get(`/admin/users`);
@@ -18,5 +19,6 @@ export const getProfile = async () => {
 export const handleLogout = () => {
     localStorage.removeItem("userProfile");
     Cookies.remove("token"); 
+    emitInfoToast("You have been logged out successfully.");
     // localStorage.removeItem("cart");
   };
